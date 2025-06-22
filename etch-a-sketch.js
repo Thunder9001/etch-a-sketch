dee6df471f3709acc3a16ac4730a62ee99de6097
Thunder9001 <30436382+Thunder9001@users.noreply.github.com>
2025-06-22 18:48:36 +0100

    Added clear grid and basic rainbow fill functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,15 +2,25 @@ const grid = document.querySelector("#grid");
 const setGridSize = document.querySelector("#setGridSize");
 let size = 16;
 let cellSize = (960 - size * 2) / size
-const divs = document.createElement("div")
+
+const colourToggle = document.querySelector("#colourToggle");
+const clearGridButton = document.querySelector("#clearGridButton");
+let blackFill = true;
+
 
 function createGrid() {
-    console.log(size, cellSize);
+    console.log(blackFill)
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
         cell.setAttribute("style", "border: 1px solid grey; width: " + cellSize + "px; height:" + cellSize + "px; margin:0px; padding: 0px;");
         cell.addEventListener("mouseover", (event) => {
-            cell.style.backgroundColor = "black";
+            if (blackFill) {
+                cell.style.backgroundColor = "black";
+            }
+            else{
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                cell.style.backgroundColor = "#" + randomColor;
+            }
         });
         grid.appendChild(cell);
     }
@@ -22,10 +32,21 @@ function clearGrid() {
     }
 }
 
+clearGridButton.addEventListener("click", () => {
+    clearGrid();
+    createGrid();
+});
+
 setGridSize.addEventListener("click", () => {
     size = parseInt(prompt("Enter a grid size between 1-100:"));
     cellSize = (960 - size * 2) / size;
-    console.log(size, cellSize);
+    clearGrid();
+    createGrid();
+});
+
+colourToggle.addEventListener("click", () => {
+    colourToggle.textContent = blackFill ? "Rainbow Fill" : "Black Fill";
+    blackFill = !blackFill;
     clearGrid();
     createGrid();
 });
